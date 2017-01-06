1a2f0d9eaa239a1b34fa6ad85d6b04ed77a5eee6
rajesha maravanthe <rraajjpoojary@gmail.com>
2017-01-06 11:33:13 +0530

    Add frond-end form to submit

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,15 @@ socket.on('message', function (message) {
     console.log('New message');
     console.log(message.text);
 });
+
+var $form = jQuery('#message-form');
+
+$form.on('submit', function (event) {
+    event.preventDefault();
+
+    socket.emit('message', {
+        text: $form.find('input[name=message]').val()
+    })
+
+    $form.find('input[name=message]').val("");
+});
